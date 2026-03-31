76c39a796d9455369b52e21c9ea7052df17d74f8
Michael Eaton <mpeaton@gmail.com>
2026-03-31 12:41:54 -0400

    initial python stuff

--- python/stateest/kalman.py
+++
@@ -1,9 +0,0 @@
-# python/stateest/kalman.py
-from ._stateest import *
-import warnings
-warnings.warn(
-    "The 'kalman' submodule is deprecated. Use 'import stateest' directly instead.",
-    DeprecationWarning,
-    stacklevel=2
-)
-
